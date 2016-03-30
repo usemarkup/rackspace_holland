17ee341a0ff581be3fd9cb0c89e2758674b9582e
Calum Brodie <calum@usemarkup.com>
2016-03-30 13:47:29 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures rackspace_holland'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'openssl', '~> 1.0'
-depends 'rackspace_mysql', '~> 6.0'
-depends 'rackspace_build_essential', '~> 2.0'
+depends 'openssl'
+depends 'rackspace_mysql'
+depends 'rackspace_build_essential'
