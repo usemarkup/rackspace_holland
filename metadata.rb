1cc5f4e8a3f41522ee58e9f58c5ffeb773dbc565
Calum Brodie <calum@usemarkup.com>
2016-03-30 13:39:53 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'openssl', '~> 1.0'
-depends 'rackspace_mysql', '~> 5.0'
+depends 'rackspace_mysql', '~> 6.0'
 depends 'rackspace_build_essential', '~> 2.0'
